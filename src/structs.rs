5f933a518c617c8f8cd26f06eec9f50b716cbaa0
MokaNiax <schmitzsemalvalentin@gmail.com>
2026-02-26 18:16:28 +0100

    ADDED: all my rust project

---
+++ src/structs.rs
@@ -0,0 +1,23 @@
+#[derive(Copy,Clone, Debug, PartialEq, Eq)]
+pub enum Type {
+    Wpawn,
+    Wrook,
+    Wknight,
+    Wbishop,
+    Wqueen,
+    Wking,
+    Bpawn,
+    Brook,
+    Bknight,
+    Bbishop,
+    Bqueen,
+    Bking,
+    None,
+}
+
+#[derive(Copy,Clone, Debug)]
+pub struct Piece {
+    pub cords: [usize; 2],
+    pub type_p: Type,
+    pub moved: bool,
+}
